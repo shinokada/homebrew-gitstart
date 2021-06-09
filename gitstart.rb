c8b474545498b665436f43adb00032f503c2d08e
Shinichi Okada <okada.shin@gmail.com>
2021-06-09 11:25:53 +0900

    version 0.2.4

--- gitstart.rb
+++ gitstart.rb
@@ -1,8 +1,8 @@
 class Gitstart < Formula
     desc "Gitstart creates, adds, and pushes a new repo with one line."
     homepage "https://github.com/shinokada/gitstart"
-    url "https://github.com/shinokada/gitstart/archive/refs/tags/v0.2.3.tar.gz"
-    sha256 "1844a32d055cd18647833d0da24062193e03c371ad55f4071e49f1b38a3fc06b"
+    url "https://github.com/shinokada/gitstart/archive/refs/tags/v0.2.4.tar.gz"
+    sha256 "5554d5f00471e448d045d396ab1fe22d898a38601c8f8aec6447ac5b4b041116"
     license "MIT"
   
     depends_on "yq"
